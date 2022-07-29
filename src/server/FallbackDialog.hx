d79290edc925d9f4269e956ce2abafa376be4fc9
Josu Igoa <josu.igoa@indaba.lks.es>
2022-07-29 13:16:26 +0200

    update dialogs interface

--- src/server/FallbackDialog.hx
+++ src/server/FallbackDialog.hx
@@ -1,8 +1,7 @@
-import api.IdeckiaApi.IdeckiaAction;
-import api.dialog.Dialog;
 import api.dialog.Dialog;
 import api.dialog.DialogTypes.Color;
 import api.dialog.DialogTypes.FileFilter;
+import api.dialog.DialogTypes.WindowOptions;
 import js.lib.Promise;
 
 @:keep
@@ -14,63 +13,69 @@ class FallbackDialog implements Dialog {
 		trace(FALLBACK_MESSAGE);
 	}
 
-	public function setOptions(options:Any) {
-		trace('Called dialog.$setOptions function.');
+	public function setDefaultOptions(options:WindowOptions) {
+		trace('Called dialog.$setDefaultOptions function.');
 		trace(FALLBACK_MESSAGE);
 	}
 
-	public function notify(title:String, text:String) {
+	public function notify(title:String, text:String, ?options:WindowOptions) {
 		trace('Called dialog.$notify function.');
 		trace(FALLBACK_MESSAGE);
 	}
 
-	public function info(title:String, text:String) {
+	public function info(title:String, text:String, ?options:WindowOptions) {
 		trace('Called dialog.$info function.');
 		trace(FALLBACK_MESSAGE);
 	}
 
-	public function error(title:String, text:String) {
+	public function warning(title:String, text:String, ?options:WindowOptions) {
+		trace('Called dialog.$warning function.');
+		trace(FALLBACK_MESSAGE);
+	}
+
+	public function error(title:String, text:String, ?options:WindowOptions) {
 		trace('Called dialog.$error function.');
 		trace(FALLBACK_MESSAGE);
 	}
 
-	public function question(title:String, text:String):Promise<Bool> {
+	public function question(title:String, text:String, ?options:WindowOptions):Promise<Bool> {
 		trace('Called dialog.$question function.');
 		trace(FALLBACK_MESSAGE);
 		return Promise.resolve(true);
 	}
 
-	public function selectFile(title:String, isDirectory:Bool = false, multiple:Bool = false, ?fileFilter:FileFilter):Promise<Array<String>> {
+	public function selectFile(title:String, isDirectory:Bool = false, multiple:Bool = false, ?fileFilter:FileFilter,
+			?options:WindowOptions):Promise<Array<String>> {
 		trace('Called dialog.$selectFile function.');
 		trace(FALLBACK_MESSAGE);
 		return Promise.resolve(['file0path', 'file1path']);
 	}
 
-	public function saveFile(title:String, ?saveName:String, ?fileFilter:FileFilter):Promise<String> {
+	public function saveFile(title:String, ?saveName:String, ?fileFilter:FileFilter, ?options:WindowOptions):Promise<String> {
 		trace('Called dialog.$saveFile function.');
 		trace(FALLBACK_MESSAGE);
 		return Promise.resolve('filepath');
 	}
 
-	public function entry(title:String, text:String, placeholder:String = ''):Promise<String> {
+	public function entry(title:String, text:String, placeholder:String = '', ?options:WindowOptions):Promise<String> {
 		trace('Called dialog.$entry function.');
 		trace(FALLBACK_MESSAGE);
 		return Promise.resolve(placeholder);
 	}
 
-	public function password(title:String, text:String, showUsername:Bool = false):Promise<Array<String>> {
+	public function password(title:String, text:String, showUsername:Bool = false, ?options:WindowOptions):Promise<Array<String>> {
 		trace('Called dialog.$password function.');
 		trace(FALLBACK_MESSAGE);
 		return Promise.resolve(['username', 'password']);
 	}
 
-	public function progress(title:String, text:String, pulsate:Bool = false, autoClose:Bool = true):Progress {
+	public function progress(title:String, text:String, pulsate:Bool = false, autoClose:Bool = true, ?options:WindowOptions):Progress {
 		trace('Called dialog.$progress function.');
 		trace(FALLBACK_MESSAGE);
 		return new FallbackProgress();
 	}
 
-	public function color(title:String, ?initialColor:String, palette:Bool = false):Promise<Color> {
+	public function color(title:String, ?initialColor:String, palette:Bool = false, ?options:WindowOptions):Promise<Color> {
 		trace('Called dialog.$color function.');
 		trace(FALLBACK_MESSAGE);
 		inline function rndColorComp()
@@ -78,13 +83,14 @@ class FallbackDialog implements Dialog {
 		return Promise.resolve(new Color({red: rndColorComp(), green: rndColorComp(), blue: rndColorComp()}));
 	}
 
-	public function calendar(title:String, text:String, ?year:UInt, ?month:UInt, ?day:UInt, ?dateFormat:String):Promise<String> {
+	public function calendar(title:String, text:String, ?year:UInt, ?month:UInt, ?day:UInt, ?dateFormat:String, ?options:WindowOptions):Promise<String> {
 		trace('Called dialog.$calendar function.');
 		trace(FALLBACK_MESSAGE);
 		return Promise.resolve(Date.now().toString());
 	}
 
-	public function list(title:String, text:String, columnHeader:String, values:Array<String>, multiple:Bool = false):Promise<Array<String>> {
+	public function list(title:String, text:String, columnHeader:String, values:Array<String>, multiple:Bool = false,
+			?options:WindowOptions):Promise<Array<String>> {
 		trace('Called dialog.$list function.');
 		trace(FALLBACK_MESSAGE);
 		return Promise.resolve(['item0', 'item1']);
